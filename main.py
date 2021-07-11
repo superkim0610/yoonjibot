ce8dd652014aa47cbb8e633a72637851c7302fed
superkim0610 <81168407+superkim0610@users.noreply.github.com>
2021-07-11 19:21:01 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 import math
 from discord.message import Message
 client = discord.Client()
-birth = [["김태윤", "0610"], ["김주함", "1120"], ["안태흠", "0328"], ["한윤찬", "0201"], ["이여명", "1004"], ["주하윤", "0930"], ["박윤지", "0208"], ["김효인", "0211"]]
+birth = [["한윤찬", "0201"],["박채진", "0206"],["박윤지", "0208"],["김효인", "0211"],["안태흠", "0328"],["신수빈", "0411"],["이서형", "0427"],["김태윤", "0610"],["전승흔", "0830"],["주하윤", "0930"],["이여명", "1004"],["윤성훈", "1019"],["공지민", "1108"],["김주함", "1120"]]
 def quadratic_equation(a,b,c):
     if a.startswith("-"):
         a=-int(a.strip("-"))
@@ -43,18 +43,25 @@ async def on_message(message):
         if len(message.content.split()) == 2:
             if message.content.split()[1] == "생일":
                 # message.content.split()[0]
-                for i in range(len(birth)-1):
+                for i in range(len(birth)):
                     # if not message.content.split()[0].find(birth[i][0]) == -1 or message.content.split()[0].find(str(birth[i][0])[1:3]) == -1:
                     if message.content.split()[0] == birth[i][0] or message.content.split()[0] == str(birth[i][0])[1:3]:
                         await message.channel.send(message.author.mention + str(birth[i][0])[1:3] + " 생일 " + getBirthString(birth[i][1]) + "!")
         if message.content.startswith("윤지야 이차방정식 "):
             if len(message.content.split()) == 5:
                 await message.channel.send("𝓍 = "+str(quadratic_equation(message.content.split()[2],message.content.split()[3],message.content.split()[4])).strip("(").strip(")").strip("0").strip("."))
-
+        if message.content.startswith("exec "):
+            r = message.content[5:]
+            await message.channel.send(exec(r))
+            print("exec : "+r)
+        if message.content.startswith("윤지야 계산해 "):
+            r = message.content[7:]
+            await message.channel.send(eval(r))
+            print("exec : "+r)
 def getBirthString(_birth):
     m = _birth[0:2]
     d = _birth[2:4]
-    return m.strip("0") + "월 " + d.strip("0") + "일"
+    return m.lstrip("0") + "월 " + d.lstrip("0") + "일"
 
 access_token = os.environ['BOT_TOKEN']
 client.run(access_token)
